51c19eaf3790cc63d3c0abc7f738ebe3799d5f72
ypayel <kukankovartem@gmail.com>
2024-07-22 13:35:28 +0200

    contact html ready

--- my-website/src/components/Contact/Contact.tsx
+++ my-website/src/components/Contact/Contact.tsx
@@ -1,8 +1,12 @@
 export const Contact = () => {
     return (
         <>
-        <div>
-            Hello
+        <div className="contact-conteiner">
+            <h1 className="contact-first-header">Contact Me</h1>
+            <p className="contact-welcometext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi alias atque sed error ipsam,
+            </p>
+            <p className="e-mail">E mail: kukankouartsiom@gmail.com</p>
+            <p className="contact-text">LinkedIn: <a href="www.linkedin.com/in/artsiom-kukankou" className="contact-link">Link</a></p>
         </div>
         </>
     )
