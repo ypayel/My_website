573e4dd7e40cab725ab6ebd2bba2582b2bed3791
ypayel <kukankovartem@gmail.com>
2024-07-28 13:46:25 +0200

    update contact

--- my-website/src/components/Contact/Contact.tsx
+++ my-website/src/components/Contact/Contact.tsx
@@ -1,12 +1,15 @@
+import "./Contact.scss";
 export const Contact = () => {
     return (
         <>
         <div className="contact-conteiner">
             <h1 className="contact-first-header">Contact Me</h1>
-            <p className="contact-welcometext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi alias atque sed error ipsam,
+            <p className="contact-welcometext">Here you will find how to contact me
             </p>
-            <p className="e-mail">E mail: kukankouartsiom@gmail.com</p>
-            <p className="contact-text">LinkedIn: <a href="www.linkedin.com/in/artsiom-kukankou" className="contact-link">Link</a></p>
+            <div className="info-conteiner">
+            <p className="e-mail">E mail: kukankouartsiom@gmail.com</p> 
+            <p className="contact-text">LinkedIn: <a href="https://www.linkedin.com/in/artsiom-kukankou/" className="contact-link" target="_blank">Link</a></p>
+            </div>
         </div>
         </>
     )
