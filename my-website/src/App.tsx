8e0624298653669321bbaf6dc837766051d6731b
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2024-08-24 13:31:20 +0200

    update

--- my-website/src/App.tsx
+++ my-website/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import Projects from './components/Projects/Projects';
 import About from './components/About/About';
