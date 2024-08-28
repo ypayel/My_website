438de5736e68a5670d58e9429e0da2bb6d3c3bb6
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2024-08-28 13:26:23 +0200

    Calculator and Inoputs in CreditCard
    fixed

--- my-website/src/components/Projects/Calculator/Calculator.tsx
+++ my-website/src/components/Projects/Calculator/Calculator.tsx
@@ -1,13 +1,13 @@
+import { useEffect, useLayoutEffect, useRef } from "react";
 import "./Calculator.scss";
 
 class CalculatorClass {
-    display: HTMLInputElement | null;
+    display: HTMLInputElement | null = null;
     currentOperand: string = '';
     previousOperand: string = '';
     operation: string | undefined;
 
     constructor() {
-        this.display = document.getElementById('display') as HTMLInputElement;
         this.clear();
     }
 
@@ -63,40 +63,44 @@ class CalculatorClass {
     }
 
     updateDisplay() {
+        if (!this.display) {
+            this.display = document.getElementById('display') as HTMLInputElement;
+        }
         if (this.display) {
             this.display.value = this.currentOperand;
         }
     }
 }
 
-const calculator = new CalculatorClass();
 
 export const Calculator = () => {
+    const calculatorRef = useRef(new CalculatorClass());
+
+    const calculator = calculatorRef.current;
+
     return (
-        <>
-            <div className="calculator">
-                <input type="text" id="display" disabled />
-                <div className="buttons">
-                    <button onClick={() => calculator.clear()} className="button-class">C</button>
-                    <button onClick={() => calculator.appendNumber('7')} className="button-class">7</button>
-                    <button onClick={() => calculator.appendNumber('8')} className="button-class">8</button>
-                    <button onClick={() => calculator.appendNumber('9')} className="button-class">9</button>
-                    <button onClick={() => calculator.appendOperator('+')} className="button-class">+</button>
-                    <button onClick={() => calculator.appendNumber('4')} className="button-class">4</button>
-                    <button onClick={() => calculator.appendNumber('5')} className="button-class">5</button>
-                    <button onClick={() => calculator.appendNumber('6')} className="button-class">6</button>
-                    <button onClick={() => calculator.appendOperator('-')} className="button-class">-</button>
-                    <button onClick={() => calculator.appendNumber('1')} className="button-class">1</button>
-                    <button onClick={() => calculator.appendNumber('2')} className="button-class">2</button>
-                    <button onClick={() => calculator.appendNumber('3')} className="button-class">3</button>
-                    <button onClick={() => calculator.appendOperator('*')} className="button-class">*</button>
-                    <button onClick={() => calculator.appendNumber('0')} className="button-class">0</button>
-                    <button onClick={() => calculator.appendNumber('.')} className="button-class">.</button>
-                    <button onClick={() => calculator.calculate()} className="button-class">=</button>
-                    <button onClick={() => calculator.appendOperator('/')} className="button-class">/</button>
-                </div>
+        <div className="calculator">
+            <input type="text" id="display" disabled />
+            <div className="buttons-calculator">
+                <button onClick={() => calculator.clear()} className="button-class">C</button>
+                <button onClick={() => calculator.appendNumber('7')} className="button-class">7</button>
+                <button onClick={() => calculator.appendNumber('8')} className="button-class">8</button>
+                <button onClick={() => calculator.appendNumber('9')} className="button-class">9</button>
+                <button onClick={() => calculator.appendOperator('+')} className="button-class">+</button>
+                <button onClick={() => calculator.appendNumber('4')} className="button-class">4</button>
+                <button onClick={() => calculator.appendNumber('5')} className="button-class">5</button>
+                <button onClick={() => calculator.appendNumber('6')} className="button-class">6</button>
+                <button onClick={() => calculator.appendOperator('-')} className="button-class">-</button>
+                <button onClick={() => calculator.appendNumber('1')} className="button-class">1</button>
+                <button onClick={() => calculator.appendNumber('2')} className="button-class">2</button>
+                <button onClick={() => calculator.appendNumber('3')} className="button-class">3</button>
+                <button onClick={() => calculator.appendOperator('*')} className="button-class">*</button>
+                <button onClick={() => calculator.appendNumber('0')} className="button-class">0</button>
+                <button onClick={() => calculator.appendNumber('.')} className="button-class">.</button>
+                <button onClick={() => calculator.calculate()} className="button-class">=</button>
+                <button onClick={() => calculator.appendOperator('/')} className="button-class">/</button>
             </div>
-        </>
+        </div>
     );
 };
 
