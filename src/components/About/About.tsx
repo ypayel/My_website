8cca1b41a00ad6d73cf84db5b470e9044ab30978
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2024-10-27 14:10:40 +0100

    response 425px

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,6 +5,9 @@ export const About = () => {
         <>
         <Nav/>
         <div className="about-container">
+        <div className="image-container">
+            <img src={process.env.PUBLIC_URL + '/images/me.jpg'} alt="me" className="my-image" />
+            </div>
             <div className="text-container">
                 <h1 className="about-header">About Me</h1>
                 <p className="about-first-text">
@@ -21,11 +24,11 @@ export const About = () => {
                     and I have a certificate of completion of the course.
                     Here below I leave a link to:  
                     <a href="https://app.diplomasafe.com/pl-PL/diploma/d859f9cfc625bf746ae5add1a94d275450f1f2760" target="_blank" className="certificate-link"> Certificate</a>
+                </p>
+                <p className="about-third-text">I also study at the WSB University in Gdansk, majoring in computer science, specializing in Front-End.
                 </p> 
             </div>
-            <div className="image-container">
-            <img src={process.env.PUBLIC_URL + '/images/me.jpg'} alt="me" className="my-image" />
-            </div>
+           
         </div>
         </>
     )
