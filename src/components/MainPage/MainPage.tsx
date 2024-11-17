193c14a19dcda5b8e2cd841ededaccba6aa3de92
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2024-11-17 11:56:55 +0100

    update

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,17 +1,23 @@
 import { Link } from "react-router-dom";
 import Nav from "../Nav/Nav";
 import "./MainPage.scss"
+import Galleria from "../Projects/Galleria/Galleria";
+import { GalleriaStorage } from "../Projects/Galleria/GalleriaStorage";
 export const MainPage = () => {
     return (
         <>
-            <Nav/>
+            <Nav/>   
+            
         <div className="welcome-text-conteiner">
             <h1 className="welcome-first-header">HI, I`M ARTEM</h1>
             <h2 className="welcome-second-header">I`M A JUNIOR FRONTEND DEVELOPER</h2>
             <p className="welcome-text">On this page you will find some information about me and also my projects, welcome
                 </p>
-                <button className="welcome-button"><Link className="welcome-button-text" to="/projects">VIEW MY PROJECTS</Link></button>
+                <button className="welcome-button"><Link className="welcome-button-text" to="/projects">VIEW MY PROJECTS</Link></button> 
         </div>
+        <div className="prev-projects">
+                <Galleria imgURL={GalleriaStorage} />
+                </div> 
         </>
     )
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -19,49 +19,7 @@ export const Projects = () => {
     <>
       <Nav />
       <div className="projects-conteiner">
-        {/* <div className="project-social">
-          <h3 className="header-projects">
-            Moj pierwszy projekt - Social Links
-          </h3>
-          <SocialLinks />
-        </div>
-        <div className="project-omlette">
-          <h3 className="header-projects">Omelette Recipe</h3>
-          <OmletRecipe />
-        </div>
-        <div className="project-creditcard">
-          <h3 className="header-projects">Credit Card Page</h3>
-          <CreditCard />
-        </div>
-        <div className="project-calculator">
-          <h3 className="header-projects">Calculator</h3>
-          <Calculator />
-        </div>
-        <div className="project-response-website">
-          <h3 className="header-projects">Response WebSite</h3>
-          <ResponseWebSite />
-        </div>
-        <div className="project-book">
-          <h3 className="header-projects">Book Store</h3>
-          <BookStore />
-        </div>
-        <div className="project-fryzjer">
-          <h3 className="header-projects">Fryzjer</h3>
-          <Fryzjer />
-        </div>
-        <div className="projects-galleria">
-          <h3 className="header-projects">Galleria</h3>
-          <Galleria imgURL={GalleriaStorage} />
-        </div>
-          <div className="projects-gazeta"> 
-            <h3 className="header-projects">Gazeta</h3>
-            <Gazeta />
-          </div>
-          <div className="projects-colorPicker">
-            <h3 className="header-projects">Color Picker</h3>
-            <ColorPicker />
-          </div> */}
-          <div>
+          <div className="links-component">
             <LinksProjects />
           </div>
       </div>
