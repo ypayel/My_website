0effc2f33a03bd246b9cc280581d8ca8a1018b22
Artsiom Kukankou <147983587+ypayel@users.noreply.github.com>
2024-12-24 12:18:14 +0100

    update

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -6,17 +6,28 @@ import { GalleriaStorage } from "../Projects/Galleria/GalleriaStorage";
 export const MainPage = () => {
     return (
         <>
-            <Nav/>   
+            <Nav/> 
+            <div>
         <div className="welcome-text-conteiner">
             <h1 className="welcome-first-header">HI, I`M ARTEM</h1>
             <h2 className="welcome-second-header">I`M A JUNIOR FRONTEND DEVELOPER</h2>
             <p className="welcome-text">On this page you will find some information about me and also my projects, welcome
                 </p>
                 <button className="welcome-button"><Link className="welcome-button-text" to="/projects">VIEW MY PROJECTS</Link></button> 
-                <div className="prev-projects">
                 </div>
+                <div className="images-container">  
+                <div className="my-stack-icons">
+                    <img src={process.env.PUBLIC_URL + `/images/social.png`} className="social-icon" alt="social" />
+                    <img src={process.env.PUBLIC_URL + `/images/react-icon.png`} className="react-icon" alt="react" />
+                    <img src={process.env.PUBLIC_URL + `/images/sass.png`} className="sass-icon" alt="sass" />
+                    <img src={process.env.PUBLIC_URL + `/images/js.png`} className="js-icon" alt="js" />
+                    <img src={process.env.PUBLIC_URL + `/images/html-5.png`} className="html-icon" alt="html"/>
+                    <img src={process.env.PUBLIC_URL + `/images/c-sharp.png`} className="c-sharp-icon" alt="c-sharp"/>
+                    <img src={process.env.PUBLIC_URL + `/images/css-3.png`} className="css-3-icon" alt="css-3" />
+                 </div>  
+                 </div>    
         </div>
-         
+        
         </>
     )
 }
